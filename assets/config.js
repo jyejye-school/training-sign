bfc88966e39c1697d973d05892a2e3d77eb53147
kusun0307-code <266050549+kusun0307-code@users.noreply.github.com>
2026-07-21 20:05:31 +0900

    Serve the complete app from Apps Script

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,5 @@
+// 기존 공개 주소의 전환 기간 호환용입니다. 새 학교는 Apps Script /exec 주소만 사용합니다.
+window.TRAINING_SIGN_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbw1GV78k69PzNKcu_HnIOcoLWiU0z3qsZ0X1E50mZA7rYoxDUMKvB3sl_sYHg4Ld1FD/exec';
 window.TRAINING_SIGN_CONFIG = Object.freeze({
-  API_URL: 'https://script.google.com/macros/s/AKfycbw1GV78k69PzNKcu_HnIOcoLWiU0z3qsZ0X1E50mZA7rYoxDUMKvB3sl_sYHg4Ld1FD/exec',
-  APP_NAME: '학교 연수 전자서명'
+  API_URL: window.TRAINING_SIGN_WEB_APP_URL
 });
